fb9e8ece09c71b1608650b267f8be09509e9a418
Arthur <A.derbin@goodt.me>
2021-10-18 22:06:07 +0500

    initial

---
+++ README.md
@@ -0,0 +1,1 @@
+# [ссылка на задание](https://github.com/netology-code/dj-homeworks/tree/drf/1.1-first-project/first_project)
---
+++ app/views.py
@@ -0,0 +1,32 @@
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+from pathlib import Path
+from datetime import datetime
+
+
+def home_view(request):
+    template_name = 'app/home.html'
+
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
+    }
+
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    current_time = datetime.now()
+    msg = f'Текущее время: {current_time}'
+    return HttpResponse(msg)
+
+g
+def workdir_view(request):
+    path = Path(f'./')
+    msg = "<br>-".join([str(p) for p in path.iterdir()])
+    msg = f'содержимое: <br> -{msg}'
+    return HttpResponse(msg)
